9f44dbf88deac9969140c7e1d5c338c815c578d0
madhubabukethineni <madhuk0391@gmail.com>
2025-10-24 01:26:15 +0530

    test4

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -4,7 +4,4 @@ output "network_self_link" {
 output "network_name" {
   value = google_compute_network.this.name
 }
-output "network_self_link" {
-  description = "The self link of the VPC network."
-  value       = google_compute_network.this.self_link
-}
+
