8ff4a5ba9dc68f44796146b50116ef84a49064de
madhubabukethineni <madhuk0391@gmail.com>
2025-10-24 01:07:32 +0530

    test_m

--- modules/gke_cluster/variables.tf
+++ modules/gke_cluster/variables.tf
@@ -1,14 +1,77 @@
-variable "project_id" { type = string }
-variable "region"     { type = string }
-variable "cluster_name" { type = string }
-variable "network_self_link" { type = string }
-variable "subnet_self_link" { type = string }
-variable "cluster_secondary_range" { type = string }
-variable "services_secondary_range" { type = string }
-variable "enable_private_nodes" { type = bool, default = true }
-variable "enable_private_endpoint" { type = bool, default = false }
-variable "master_ipv4_cidr_block" { type = string, default = null }
-variable "workload_identity" { type = bool, default = true }
-variable "logging_service"    { type = string, default = "logging.googleapis.com/kubernetes" }
-variable "monitoring_service" { type = string, default = "monitoring.googleapis.com/kubernetes" }
-variable "release_channel" { type = string, default = "" }
+variable "project_id" {
+  description = "The ID of the GCP project where the cluster will be created"
+  type        = string
+}
+
+variable "region" {
+  description = "The region in which the cluster will be created"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+  type        = string
+}
+
+variable "network_self_link" {
+  description = "The self link of the VPC network"
+  type        = string
+}
+
+variable "subnet_self_link" {
+  description = "The self link of the subnet"
+  type        = string
+}
+
+variable "cluster_secondary_range" {
+  description = "The secondary range name for cluster pods"
+  type        = string
+}
+
+variable "services_secondary_range" {
+  description = "The secondary range name for cluster services"
+  type        = string
+}
+
+variable "enable_private_nodes" {
+  description = "Whether to enable private nodes for the cluster"
+  type        = bool
+  default     = true
+}
+
+variable "enable_private_endpoint" {
+  description = "Whether the master's endpoint is private"
+  type        = bool
+  default     = false
+}
+
+variable "master_ipv4_cidr_block" {
+  description = "The IP range in CIDR notation for the master"
+  type        = string
+  default     = null
+}
+
+variable "workload_identity" {
+  description = "Whether to enable Workload Identity"
+  type        = bool
+  default     = true
+}
+
+variable "logging_service" {
+  description = "The logging service to use for the GKE cluster"
+  type        = string
+  default     = "logging.googleapis.com/kubernetes"
+}
+
+variable "monitoring_service" {
+  description = "The monitoring service to use for the GKE cluster"
+  type        = string
+  default     = "monitoring.googleapis.com/kubernetes"
+}
+
+variable "release_channel" {
+  description = "The release channel for the cluster (e.g., RAPID, REGULAR, STABLE)"
+  type        = string
+  default     = ""
+}
+
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,27 @@
-variable "project_id" { type = string }
-variable "name"       { type = string }
-variable "auto_create_subnetworks" { type = bool, default = false }
-variable "routing_mode" { type = string, default = "REGIONAL" }
-variable "description" { type = string, default = "" }
+variable "project_id" {
+  description = "The ID of the GCP project where the VPC network will be created."
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the VPC network."
+  type        = string
+}
+
+variable "auto_create_subnetworks" {
+  description = "When set to true, the network is created in auto mode with default subnets. Disable for custom subnet creation."
+  type        = bool
+  default     = false
+}
+
+variable "routing_mode" {
+  description = "The network-wide routing mode. Valid values are REGIONAL or GLOBAL."
+  type        = string
+  default     = "REGIONAL"
+}
+
+variable "description" {
+  description = "Optional description for the network."
+  type        = string
+  default     = ""
+}
