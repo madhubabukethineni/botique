bbe18b6d86a184fd8d9036d4e31339f8d26d4d13
madhubabukethineni <madhuk0391@gmail.com>
2025-10-24 00:45:01 +0530

    test

--- envs/main/gke/variables.tf
+++ envs/main/gke/variables.tf
@@ -1,22 +1,70 @@
+# ==============================
+# GKE Cluster Module Variables
+# ==============================
+
 variable "project_id" {
-     type = string
+  description = "The GCP project ID where the cluster will be created"
+  type        = string
 }
+
 variable "region" {
-     type = string
+  description = "The region where the GKE cluster will be deployed"
+  type        = string
 }
+
 variable "cluster_name" {
-     type = string
+  description = "The name of the GKE cluster"
+  type        = string
 }
+
 variable "cluster_secondary_range" {
-     type = string
+  description = "The name of the secondary IP range for pods"
+  type        = string
 }
+
 variable "services_secondary_range" {
-     type = string
+  description = "The name of the secondary IP range for services"
+  type        = string
 }
+
 variable "master_ipv4_cidr_block" {
-     type = string
+  description = "The CIDR block for the GKE control plane"
+  type        = string
+  default     = null
 }
+
+variable "enable_private_nodes" {
+  description = "Enable private nodes in the cluster"
+  type        = bool
+  default     = true
+}
+
+variable "enable_private_endpoint" {
+  description = "Enable private endpoint for the cluster"
+  type        = bool
+  default     = false
+}
+
+variable "workload_identity" {
+  description = "Enable Workload Identity for the cluster"
+  type        = bool
+  default     = true
+}
+
+variable "logging_service" {
+  description = "Logging service used by the cluster"
+  type        = string
+  default     = "logging.googleapis.com/kubernetes"
+}
+
+variable "monitoring_service" {
+  description = "Monitoring service used by the cluster"
+  type        = string
+  default     = "monitoring.googleapis.com/kubernetes"
+}
+
 variable "release_channel" {
-     type = string
-     default = ""
+  description = "Release channel for GKE (e.g., RAPID, REGULAR, STABLE)"
+  type        = string
+  default     = ""
 }
