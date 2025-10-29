d44a7cfc4080a2cd36ec33a54e8537f6d0e1c7ac
madhubabukethineni <madhuk0391@gmail.com>
2025-10-29 18:19:55 +0530

    fix1

--- environments/prod/node_pool/main.tf
+++ environments/prod/node_pool/main.tf
@@ -6,6 +6,6 @@ module "node_pool" {
   source = "../../../modules/node_pool"
   project_id = var.project_id
   region = var.region
-  cluster_name = data.terraform_remote_state.gke_state.outputs.cluster_name
+  cluster_name = var.cluster_name
   node_pools = var.node_pools
 }
--- environments/prod/node_pool/variables.tf
+++ environments/prod/node_pool/variables.tf
@@ -8,3 +8,7 @@ variable "node_pools" {
     type = list(any) 
     default = []
 }
+variable "cluster_name" {
+  description = "The name of the GKE cluster to which the node pools will be added."
+  type        = string
+}
