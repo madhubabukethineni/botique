2d752afceac08da60f2c409b6d6e0da3069642a2
madhubabukethineni <madhuk0391@gmail.com>
2025-12-27 19:15:43 +0530

    subnet

--- environments/prod/subnet/main.tf
+++ environments/prod/subnet/main.tf
@@ -8,9 +8,9 @@ module "subnets" {
 
   name                     = each.value.name
   ip_cidr_range            = each.value.ip_cidr_range
-  network                  = each.value.network_name
+  network_name             = each.value.network_name
   region                   = each.value.region
-  project                  = each.value.project_id
+  project_id               = each.value.project_id
   description               = each.value.description
   enable_flow_logs         = each.value.enable_flow_logs
   private_ip_google_access = each.value.private_ip_google_access
