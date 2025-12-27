65760326b10f7b9284c6603d7c8004770d0a2c39
madhubabukethineni <madhuk0391@gmail.com>
2025-12-27 21:58:58 +0530

    gke

--- environments/prod/gke/variables.tf
+++ environments/prod/gke/variables.tf
@@ -1,19 +1,25 @@
+# variable "gke_clusters" {
+#   type = map(object({
+#     project_id                 = string
+#     location                   = string
+#     network                    = string
+#     subnetwork                 = string
+#     enable_autopilot           = bool
+#     release_channel            = optional(string)
+#     ip_allocation_policy       = optional(any)
+#     private_cluster_config     = optional(any)
+#     master_authorized_networks = optional(list(any), [])
+#     workload_identity          = optional(bool, false)
+#     addons_config              = optional(any)
+#     network_policy_config      = optional(any)
+#     logging_components         = optional(list(string), ["SYSTEM_COMPONENTS"])
+#     monitoring_components      = optional(list(string), ["SYSTEM_COMPONENTS"])
+#     node_pools                 = optional(any, {})
+#   }))
+# }
+
+
 variable "gke_clusters" {
-  type = map(object({
-    project_id                 = string
-    location                   = string
-    network                    = string
-    subnetwork                 = string
-    enable_autopilot           = bool
-    release_channel            = optional(string)
-    ip_allocation_policy       = optional(any)
-    private_cluster_config     = optional(any)
-    master_authorized_networks = optional(list(any), [])
-    workload_identity          = optional(bool, false)
-    addons_config              = optional(any)
-    network_policy_config      = optional(any)
-    logging_components         = optional(list(string), ["SYSTEM_COMPONENTS"])
-    monitoring_components      = optional(list(string), ["SYSTEM_COMPONENTS"])
-    node_pools                 = optional(any, {})
-  }))
+  type = any
+  description = "Map of GKE cluster configurations"
 }
